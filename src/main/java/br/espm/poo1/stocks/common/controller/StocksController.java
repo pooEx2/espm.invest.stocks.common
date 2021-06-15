223cc54e2a7e287e6d78e893b86e781832f78613
vitor-tk <62676267+Vitor-TK@users.noreply.github.com>
2021-06-15 15:47:51 -0300

    mudanca no controller

--- src/main/java/br/espm/poo1/stocks/common/controller/StocksController.java
+++ src/main/java/br/espm/poo1/stocks/common/controller/StocksController.java
@@ -3,12 +3,14 @@ package br.espm.poo1.stocks.common.controller;
 import br.espm.poo1.stocks.common.datatype.Price;
 import br.espm.poo1.stocks.common.datatype.Stocks;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @FeignClient("stocks-service")
+@Controller
 public interface StocksController {
 
     @GetMapping("/price")
@@ -22,6 +24,13 @@ public interface StocksController {
 
     @GetMapping("/stocks/{id}")
     Stocks stocks(String id);
+
+    @GetMapping("/stocks/{id}/{symbol}/{name}")
+    List<Stocks> stocks(
+            @PathVariable String id,
+            @PathVariable String symbol,
+            @PathVariable String name
+    );
 
     @GetMapping("/stocks/{symbol}/{date}")
     Stocks stocks(
